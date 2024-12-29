adb533b626f8f074c3e71d27787b90eee0b3816e
Enting <76424122+Enting-Chen@users.noreply.github.com>
2024-12-29 23:46:27 +0000

    link

--- src/CalendarContainer.js
+++ src/CalendarContainer.js
@@ -7,7 +7,7 @@ const CalendarContainer = ({ companyId }) => {
   useEffect(() => {
     const fetchCompanyData = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/companies/${companyId}`);
+        const response = await fetch(`https://calender-backend-iota.vercel.app/api/companies?id=${companyId}`);
         const data = await response.json();
         setColor(data.color);
       } catch (error) {
