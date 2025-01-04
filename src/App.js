1448d33171de1a915ca2340492e9bd221527d5d7
Enting <76424122+Enting-Chen@users.noreply.github.com>
2025-01-04 00:37:25 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 import CalendarComponent from './CalendarComponent';
+import { Button, Calendar, CalendarCell, CalendarGrid, Heading } from 'react-aria-components';
 
 function App() {
   return (
@@ -14,7 +15,19 @@ function App() {
 
 function CalendarWithCompany() {
   const { company } = useParams();
-  return <CalendarComponent />;
+  // return <CalendarComponent />;
+  return (
+    <Calendar aria-label="Appointment date">
+      <header>
+        <Button slot="previous">◀</Button>
+        <Heading />
+        <Button slot="next">▶</Button>
+      </header>
+      <CalendarGrid>
+        {(date) => <CalendarCell date={date} />}
+      </CalendarGrid>
+    </Calendar>
+  );
 }
 
 export default App;
