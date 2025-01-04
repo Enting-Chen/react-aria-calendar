6adba64b9e79ab4c244f6d278e1f87ef8f70c4b4
Enting Chen <enting_chen@outlook.com>
2025-01-04 03:08:29 +0000

    test

--- src/CalendarComponent.js
+++ src/CalendarComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {parseDate} from '@internationalized/date';
 import { Button, Calendar, CalendarCell, CalendarGrid, Heading } from 'react-aria-components';
-import './index.css';
 
 function CalendarComponent() {
   return (
