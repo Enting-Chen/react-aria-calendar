e9ac3ad2758edac9fb81bfe6d62f5be96bb887b4
Enting <76424122+Enting-Chen@users.noreply.github.com>
2025-01-04 00:23:59 +0000

    remove old calendar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import CalendarContainer from './CalendarContainer';
 import CalendarComponent from './CalendarComponent';
 
 function App() {
@@ -15,14 +14,7 @@ function App() {
 
 function CalendarWithCompany() {
   const { company } = useParams();
-  return (
-    <div>
-      <h1>React Calendar Example</h1>
-      <CalendarContainer companyId={company} />
-      <h1>React Calendar Example</h1>
-      <CalendarComponent />
-    </div>
-  );
+  return <CalendarComponent />;
 }
 
 export default App;
