7ae8a36af1f4a4bb0f77a95be38bda79d0a21dca
Enting Chen <enting_chen@outlook.com>
2025-01-04 02:52:18 +0000

    test

--- src/CalendarComponent.js
+++ src/CalendarComponent.js
@@ -1,17 +1,16 @@
-import {parseDate} from '@internationalized/date';
 import React from 'react';
 import { Button, Calendar, CalendarCell, CalendarGrid, Heading } from 'react-aria-components';
 
 function CalendarComponent() {
   return (
-    <Calendar aria-label="Appointment date" value={parseDate('2020-02-03')} >
+    <Calendar aria-label="Appointment date" defaultValue={parseDate('2020-02-03')} style={{ color: 'var(--blue)' }}>
       <header>
-        <Button slot="previous" >◀</Button>
-        <Heading />
-        <Button slot="next" >▶</Button>
+        <Button slot="previous" style={{ color: 'var(--blue)' }}>◀</Button>
+        <Heading style={{ color: 'var(--blue)' }}/>
+        <Button slot="next" style={{ color: 'var(--blue)' }}>▶</Button>
       </header>
       <CalendarGrid>
-        {date => <CalendarCell />}
+        {date => <CalendarCell date={date} style={{ color: 'var(--blue)' }}/>}
       </CalendarGrid>
     </Calendar>
   );
