25bbe10b12a40da222c6c28acc6d1ddf5d8685f3
Enting Chen <enting_chen@outlook.com>
2025-01-04 02:45:07 +0000

    test

--- src/CalendarComponent.js
+++ src/CalendarComponent.js
@@ -4,14 +4,14 @@ import { Button, Calendar, CalendarCell, CalendarGrid, Heading } from 'react-ari
 
 function CalendarComponent() {
   return (
-    <Calendar aria-label="Appointment date" style={{ color: 'var(--blue)' }}>
+    <Calendar aria-label="Appointment date" defaultValue={parseDate('2020-02-03')} style={{ color: 'var(--blue)' }}>
       <header>
         <Button slot="previous" style={{ color: 'var(--blue)' }}>◀</Button>
         <Heading style={{ color: 'var(--blue)' }}/>
         <Button slot="next" style={{ color: 'var(--blue)' }}>▶</Button>
       </header>
       <CalendarGrid>
-        {date => <CalendarCell date={parseDate('2020-02-03')} style={{ color: 'var(--blue)' }}/>}
+        {date => <CalendarCell style={{ color: 'var(--blue)' }}/>}
       </CalendarGrid>
     </Calendar>
   );
